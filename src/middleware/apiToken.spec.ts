432cdbbbe06d1ecc313d52e0cc613eadc989ebd9
Marc Marina Miravitlles <marc.marina.miravitlles@gmail.com>
2021-01-19 16:49:15 +0000

    Added more tests

--- src/middleware/apiToken.spec.ts
+++ src/middleware/apiToken.spec.ts
@@ -2,16 +2,41 @@ import 'dotenv/config';
 
 import apiToken, { checkToken } from './apiToken';
 
-test('middleware calls next when the token is present and valid', () => {
+test('middleware calls next without an exception when the token is present and valid', () => {
   const req = {
     headers: {
       'x-api-token': process.env.API_TOKEN,
     },
   };
-  const next = jest.fn();
+  const next = jest.fn(ex => {
+    expect(ex).not.toBeDefined();
+  });
 
   apiToken(req, {}, next);
-  expect(next).toBeCalled();
+});
+
+test('middleware calls next with an exception when the token is invalid', () => {
+  const req = {
+    headers: {
+      'x-api-token': 'invalidtoken',
+    },
+  };
+  const next = jest.fn(ex => {
+    expect(ex).toEqual({ msg: 'API Token is not valid', status: 401 });
+  });
+
+  apiToken(req, {}, next);
+});
+
+test('middleware calls next with an exception when the token is not provided', () => {
+  const req = {
+    headers: {},
+  };
+  const next = jest.fn(ex => {
+    expect(ex).toEqual({ msg: 'No API Token provided', status: 401 });
+  });
+
+  apiToken(req, {}, next);
 });
 
 test('throws error when no token is present', () => {

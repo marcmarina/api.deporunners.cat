1357d301bbad43a00a19f7603a72820108c04405
Marc Marina Miravitlles <marc.marina.miravitlles@gmail.com>
2022-06-10 01:01:39 +0100

    Fixed wrong test name

--- src/config/config.spec.ts
+++ src/config/config.spec.ts
@@ -19,7 +19,7 @@ describe('variable fetching', () => {
       expect(fetchVariable(DEFINED)).toStrictEqual('test');
     });
 
-    it('should return null when fetching an undefined environment variable', () => {
+    it('should throw an error when fetching an undefined environment variable', () => {
       expect(() => fetchVariable(NOT_DEFINED)).toThrow(
         `Could not fetch environment variable ${NOT_DEFINED}`
       );

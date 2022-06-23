d6356fb41c833a44f32789fe8744c6c872e6afbd
Marc Marina <mmarina@findmypast.com>
2022-06-23 15:35:16 +0200

    Added types to request logger middleware

--- src/middleware/request-logging.ts
+++ src/middleware/request-logging.ts
@@ -1,8 +1,8 @@
 import logger from '../utils/logger';
 import Context from '../utils/Context';
-import { Request } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
-export default (req: Request, _res, next) => {
+export default (req: Request, _res: Response, next: NextFunction) => {
   logger.request({
     user: Context.getUserId(),
     ip: req.ip,

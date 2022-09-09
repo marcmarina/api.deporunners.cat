6dc4397d707dab7db6550954b8ee68f2873871f4
Marc Marina <marc.marina.miravitlles@gmail.com>
2022-09-09 22:17:16 +0100

    Removed unused util functions

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -2,23 +2,11 @@ import crypto from 'crypto';
 import pug from 'pug';
 import pathNode from 'path';
 import CSSInliner from 'css-inliner';
-import fs from 'fs';
 import { Document } from 'mongoose';
 
 export const generateToken = (size: number) => {
   return crypto.randomBytes(size).toString('hex');
 };
-
-export const mapToJSON = (map: Map<any, any>) => {
-  const obj = {};
-  map.forEach((value, key) => {
-    obj[key] = value;
-  });
-  return obj;
-};
-
-export const paginateArray = (arr: any[], page: number, pageSize: number) =>
-  arr.slice((page - 1) * pageSize, page * pageSize);
 
 export const getModelName = (model: Document) =>
   (model.constructor as unknown as Document).modelName;
@@ -36,9 +24,3 @@ export const getPugTemplate = async (path: string, data?: any) => {
 
 export const randomInt = (min: number, max: number) =>
   Math.random() * (max - min + 1) + min;
-
-export const deleteFile = (filePath: string) => {
-  fs.unlink(filePath, (err) => {
-    if (err) throw err;
-  });
-};

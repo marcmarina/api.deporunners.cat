860c3c8794bfd0d0f477fabf68cec948c7135bee
Marc Marina <marc.marina.miravitlles@gmail.com>
2022-12-11 14:20:26 +0000

    Added generate-session unit test

---
+++ src/authentication/generate-session.spec.ts
@@ -0,0 +1,82 @@
+import { generateSession } from './generate-session';
+import { signJWT, validateJWT } from './jwt-utils';
+import { getRefreshTokenOwner } from './get-refresh-token-owner';
+
+jest.mock('./jwt-utils');
+jest.mock('./get-refresh-token-owner');
+
+describe('generateSession', () => {
+  const mockedValidateJWT = validateJWT as jest.Mock;
+  const mockedSignJWT = signJWT as jest.Mock;
+
+  const mockedGetRefreshTokenOwner = getRefreshTokenOwner as jest.Mock;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('generates a session when the user has a valid auth and refresh token', async () => {
+    const authToken = 'authToken';
+    const refreshToken = 'refreshToken';
+
+    mockedValidateJWT.mockReturnValueOnce({ id: 1 });
+
+    const session = generateSession(authToken, refreshToken);
+
+    await expect(session).resolves.toEqual({
+      user: {
+        id: 1,
+      },
+      refreshToken,
+      authToken,
+    });
+  });
+
+  it("generates a session when the user doesn't have a valid auth but has a valid refresh token", async () => {
+    const authToken = 'authToken';
+    const refreshToken = 'refreshToken';
+
+    mockedValidateJWT.mockReturnValueOnce(null);
+    mockedGetRefreshTokenOwner.mockReturnValueOnce({ id: 1 });
+    mockedSignJWT.mockReturnValueOnce('authToken');
+
+    const session = generateSession(authToken, refreshToken);
+
+    await expect(session).resolves.toEqual({
+      user: {
+        id: 1,
+      },
+      refreshToken,
+      authToken,
+    });
+  });
+
+  it("returns null when the user doesn't have valid tokens", async () => {
+    const authToken = 'authToken';
+    const refreshToken = 'refreshToken';
+
+    mockedValidateJWT.mockReturnValueOnce(null);
+    mockedGetRefreshTokenOwner.mockReturnValueOnce(null);
+
+    const session = generateSession(authToken, refreshToken);
+
+    await expect(session).resolves.toEqual({
+      authToken: null,
+      refreshToken: null,
+      user: null,
+    });
+  });
+
+  it("returns null when the user doesn't have any tokens", async () => {
+    const authToken = null;
+    const refreshToken = null;
+
+    const session = generateSession(authToken, refreshToken);
+
+    await expect(session).resolves.toEqual({
+      authToken: null,
+      refreshToken: null,
+      user: null,
+    });
+  });
+});

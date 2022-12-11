7833f186f6c77806630c585c48de11aefeab9f56
Marc Marina <marc.marina.miravitlles@gmail.com>
2022-12-11 14:44:11 +0000

    Added jwt-utils unit test

---
+++ src/authentication/jwt-utils.spec.ts
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+import { validateJWT } from './jwt-utils';
+
+jest.mock('jsonwebtoken');
+
+describe('validateJWT', () => {
+  const mockedJwt = {
+    verify: jwt.verify as jest.Mock,
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('returns the decoded token when the token is valid', () => {
+    const token = 'token';
+
+    mockedJwt.verify.mockReturnValueOnce({ id: 1 });
+
+    const decodedToken = validateJWT(token);
+
+    expect(decodedToken).toEqual({ id: 1 });
+  });
+
+  it('returns null when the token is not valid', () => {
+    const token = 'token';
+
+    mockedJwt.verify.mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const decodedToken = validateJWT(token);
+
+    expect(decodedToken).toBeNull();
+  });
+});

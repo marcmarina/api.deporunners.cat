7a90308397f27fa3acc2c9f101445ed2f7e5b079
Marc Marina <marc.marina.miravitlles@gmail.com>
2023-02-18 17:54:22 +0000

    Added BaseService class

---
+++ src/services/base-service.ts
@@ -0,0 +1,24 @@
+import { ServiceError } from '../errors/errors';
+
+export class BaseService {
+  constructor() {
+    return new Proxy(this, {
+      get: (target, prop) => {
+        const method = target[prop];
+        if (typeof method !== 'function') return method;
+
+        return async (...args: any[]) => {
+          try {
+            return await method.apply(target, args);
+          } catch (err) {
+            throw new ServiceError({
+              message: err.message,
+              method: prop as string,
+              service: target.constructor.name,
+            });
+          }
+        };
+      },
+    });
+  }
+}

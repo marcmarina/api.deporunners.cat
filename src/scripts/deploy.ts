93e971507ad7aa0f6e353778bb36e232b3d5e5e3
Marc Marina <marc.marina.miravitlles@gmail.com>
2023-02-19 17:49:09 +0000

    Removed fire-webhook module

--- src/scripts/deploy.ts
+++ src/scripts/deploy.ts
@@ -1,6 +1,6 @@
-import { logger } from '../logger';
+import axios, { Method } from 'axios';
 
-import { fireWebhook } from './fire-webhook';
+import { logger } from '../logger';
 
 async function deploy() {
   const [deploymentWebhookUrl] = process.argv.slice(2);
@@ -19,6 +19,45 @@ async function deploy() {
     throw new Error(
       `Deployment webhook failed with status ${result.status} and response ${result.response}`,
     );
+  }
+}
+
+type Maybe<T> = T | null;
+
+async function fireWebhook(
+  method: Method = 'POST',
+  url,
+): Promise<{
+  success: boolean;
+  error: Maybe<Error>;
+  response: Maybe<any>;
+  status: Maybe<number>;
+}> {
+  try {
+    const response = await axios({ method, url });
+
+    if (response.status === 200) {
+      return {
+        success: true,
+        error: null,
+        response: response.data,
+        status: response.status,
+      };
+    } else {
+      return {
+        success: false,
+        response: response.data,
+        status: response.status,
+        error: null,
+      };
+    }
+  } catch (error) {
+    return {
+      success: false,
+      error: error,
+      response: null,
+      status: null,
+    };
   }
 }
 

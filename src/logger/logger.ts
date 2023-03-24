baad4916dcd325e759fac12ead77d432e078ff16
Marc Marina <marc.marina.miravitlles@gmail.com>
2023-03-24 22:26:48 +0000

    Made logger into a class

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -6,7 +6,7 @@ import { createLogger } from './create-logger';
 
 const pinoLogger = createLogger();
 
-export const logger = {
+class Logger {
   /**
    * Log an error. If the environment is development or testing it only logs it to the console. Otherwise it also logs it to Sentry.
    *
@@ -15,17 +15,21 @@ export const logger = {
    * @param {Error} error - Error to log.
    * @param {boolean} [logToSentry] - Flag to bypass environment and force logging to Sentry.
    */
-  error: (error: Error, logToSentry?: boolean) => {
+  error(error: Error, logToSentry?: boolean) {
     pinoLogger.error(error);
 
     if (logToSentry || (!envIsDev() && !envIsTest())) {
       Sentry.captureException(error);
     }
-  },
-  debug: (msg: string, args?: Record<string, unknown>) => {
+  }
+
+  debug(msg: string, args?: Record<string, unknown>) {
     pinoLogger.debug({ ...args }, msg);
-  },
-  info: (msg: string, args?: Record<string, unknown>) => {
+  }
+
+  info(msg: string, args?: Record<string, unknown>) {
     pinoLogger.info({ ...args }, msg);
-  },
-};
+  }
+}
+
+export const logger = new Logger();

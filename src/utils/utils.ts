03f6be3daa1bb072ee8a9d1b2e400a500c0156f5
Marc Marina <mmarina@findmypast.com>
2023-04-19 10:47:01 +0100

    Using isNumber function to test if internal packages work

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,9 @@
+import { isNumber } from '@deporunners/utils';
+
 export function randomInt(min: number, max: number) {
   return Math.random() * (max - min + 1) + min;
+}
+
+export function isANumber(value: unknown): value is number {
+  return isNumber(value);
 }

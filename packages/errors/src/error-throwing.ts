19864ca96bfaa0d402421c1e60ac328784f50432
Marc Marina <mmarina@findmypast.com>
2023-04-19 16:39:22 +0100

    Added docs to checkForErrors

--- packages/errors/src/error-throwing.ts
+++ packages/errors/src/error-throwing.ts
@@ -1,8 +1,13 @@
+import { Request } from 'express';
 import { validationResult } from 'express-validator';
 
 import { InputError } from './errors';
 
-export const checkForErrors = (req) => {
+/**
+ * Checks for express-validator errors and throws an InputError if any are found.
+ * @param req Request object
+ */
+export function checkForErrors(req: Request) {
   const errors = validationResult(req);
   if (errors.array().length > 0) throw new InputError(errors);
-};
+}

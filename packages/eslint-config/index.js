ca86bac87ea75dca98dd944dcf289fb2451bfb39
Marc Marina <mmarina@findmypast.com>
2023-04-19 12:09:31 +0100

    Added eslint-config package

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:jest/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // Typescript
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '_' }],
+
+    'no-return-await': 'off',
+    'no-console': 'error',
+    'no-unreachable': 'error',
+
+    // Imports
+    'import/no-cycle': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};

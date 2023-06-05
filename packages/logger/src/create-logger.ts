70133c41c7b67eec74dbc1376a20f081ea10a44b
Marc Marina <mmarina@findmypast.com>
2023-06-05 14:03:06 +0100

    Fixed unit tests

--- packages/logger/src/create-logger.ts
+++ packages/logger/src/create-logger.ts
@@ -1,20 +1,27 @@
 import pino from 'pino';
 
-import { envIsDev, envIsTest } from '@deporunners/config';
+import { config, Environment } from '@deporunners/config';
+import { assertNever } from '@deporunners/utils';
 
 function getLogLevel(): pino.LevelWithSilent {
-  if (envIsDev()) {
-    return 'trace';
-  } else if (envIsTest()) {
-    return 'silent';
-  } else {
-    return 'info';
+  switch (config.environment) {
+    case Environment.Test:
+      return 'silent';
+    case Environment.Dev:
+      return 'trace';
+    case Environment.Staging:
+    case Environment.Production:
+      return 'info';
+    default:
+      assertNever(config.environment);
   }
 }
 
 export function createLogger() {
   return pino({
-    ...(envIsDev() && { transport: { target: 'pino-pretty' } }),
+    ...(config.environment === Environment.Dev && {
+      transport: { target: 'pino-pretty' },
+    }),
     level: getLogLevel(),
   });
 }
